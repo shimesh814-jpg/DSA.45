2c627de9232b2e3e20440b8aa51adaf14d7311be
Himesh <shimesh814@gmail.com>
2026-02-07 18:28:59 +0530

    Add files via upload

---
+++ Basic of maths/Reverse of number.cpp
@@ -0,0 +1,15 @@
+# include<iostream>
+using namespace std;
+int main()
+{
+      int num;
+      cin>>num;
+       int revnum = 0;
+       while(num>0)
+       {
+        int lastdigit=num%10;
+         revnum = (revnum * 10)+lastdigit;
+         num =num/10;    
+       }
+       cout<< revnum;
+}
