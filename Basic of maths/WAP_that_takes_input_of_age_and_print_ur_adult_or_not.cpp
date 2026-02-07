65d17e539c26743d492b42abfb17bdd714532c49
Himesh <shimesh814@gmail.com>
2026-02-07 18:29:32 +0530

    Add files via upload

---
+++ Basic of maths/WAP_that_takes_input_of_age_and_print_ur_adult_or_not.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int age;
+    cin>>age; 
+    if(age>= 18)
+    {
+        cout<<"adult";
+    }  
+    else{
+        cout<<"not adult";
+    }
+    return 0;
+}
