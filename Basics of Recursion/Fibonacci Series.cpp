b9beefec76233cc3df28611cd98782a28ba7dfdf
Himesh <shimesh814@gmail.com>
2026-02-11 07:56:09 +0530

    Add files via upload

---
+++ Basics of Recursion/Fibonacci Series.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+// Recursive function
+int fib(int n)
+{
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+
+    return fib(n - 1) + fib(n - 2);
+}
+
+int main()
+{
+    int n;
+    cout << "Enter number of terms: ";
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << fib(i) << " ";
+    }
+
+    return 0;
+}
