9be09e90fdfd240000a0f31986eeaf3362d44764
Himesh <shimesh814@gmail.com>
2026-03-25 08:39:23 +0530

    Add files via upload

---
+++ Array/Easy/Remove duplicates from Sorted array.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{ 
+    int n;
+    cout << "Enter the value of n" << endl;
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter the elements in the array" << endl;
+
+    for(int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int i = 0;
+
+    for(int j = 1; j < n; j++)   // start from 1
+    {
+        if(arr[j] != arr[j - 1])  // compare with previous element
+        {
+            arr[i + 1] = arr[j];
+            i++;
+        }
+    }
+
+    cout << "Number of unique elements: " << i + 1 << endl;
+
+    return 0;
+}
