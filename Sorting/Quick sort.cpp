5e13c687f8a9dbbd06ec0afa549be9696aefa63b
Himesh <shimesh814@gmail.com>
2026-03-30 18:43:01 +0530

    Update Quick sort.cpp

--- Sorting/Quick sort.cpp
+++ Sorting/Quick sort.cpp
@@ -58,7 +58,7 @@ int main() {
     cout<<"enter the value of n:"<<endl;
     cin>>n;
     vector<int> arr(n);
-    cout<<"Enter the elements in the array :"<<endl;;
+    cout<<"Enter the elements in the array :"<<endl;
     for(int i=0;i<n;i++)
     {
         cin>>arr[i];
